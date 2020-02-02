e08e447bffef7eee29518bbff33b8844d9354fbb
DATALOVERVISUALIZER <47347284+DATALOVERVISUALIZER@users.noreply.github.com>
2020-02-03 01:17:50 +0300

    ad

--- BOOKS/Sertificated_courses_documents/BIG DATA/Handover_Training/js/data_deep.js
+++
@@ -1,7 +0,0 @@
-sankeyDraw(
-  {
-    'nodes': [{"name": "CreditCard_LogTxtCash advance 2", "id": "0"}, {"name": "ChurningEvent 1", "id": "1"}],
-    'links': [{"source": 0, "target": 1, "value": 1479}],
-    'sinksRight': false
-  }
-);
